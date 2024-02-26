14ee125d5e7404227c805be3187fcc26d446cf96
UrThings <160115501+UrThings@users.noreply.github.com>
2024-02-26 14:48:52 +0800

    Update tusuliin-medeelel.js

--- tusuliin-medeelel.js
+++ tusuliin-medeelel.js
@@ -2,27 +2,27 @@
 export const tusuluud = [{
   name: '2р хичээл',
   title: 'Анхны программ',
-  image: "Quiz-Kid.jpg",
+  image: "OIP.jpg",
   number: '101'
 },{
   name: '2р хичээл',
   title: 'Анхны программ',
-  image: "Quiz-Kid.jpg",
+  image: "OIP.jpg",
   number: '102'
 },{
   name: '2р хичээл',
   title: 'Анхны программ',
-  image: "Quiz-Kid.jpg",
+  image: "OIP.jpg",
   number: '103'
 },{
   name: '2р хичээл',
   title: 'Анхны программ',
-  image: "Quiz-Kid.jpg",
+  image: "OIP.jpg",
   number: '104'
 },{
   name: '2р хичээл',
   title: 'Анхны программ',
-  image: "Quiz-Kid.jpg",
+  image: "OIP.jpg",
   number: '105'
 }];
 
