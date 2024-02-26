2203128a3bc12f3ae04b3303b942817c60693050
UrThings <160115501+UrThings@users.noreply.github.com>
2024-02-26 15:06:21 +0800

    Update tusuliin-medeelel.js

--- tusuliin-medeelel.js
+++ tusuliin-medeelel.js
@@ -1,27 +1,27 @@
 
 export const tusuluud = [{
-  name: '2р хичээл',
-  title: 'Анхны программ',
+  name: '1р төсөл',
+  title: 'Дэлгэрэнгүй if нөхцөл шалгах үйлдэл - жижиг програм',
   image: "OIP.jpg",
   number: '101'
 },{
-  name: '2р хичээл',
-  title: 'Анхны программ',
+  name: '2р төсөл',
+  title: 'Жижиг чатын програм хийх',
   image: "OIP.jpg",
   number: '102'
 },{
-  name: '2р хичээл',
-  title: 'Анхны программ',
+  name: '3р төсөл',
+  title: 'Онгоц бууддаг тоглоом',
   image: "OIP.jpg",
   number: '103'
 },{
-  name: '2р хичээл',
-  title: 'Анхны программ',
+  name: '4р төсөл',
+  title: 'Толь бичгийн програм зохиох',
   image: "OIP.jpg",
   number: '104'
 },{
-  name: '2р хичээл',
-  title: 'Анхны программ',
+  name: '5р төсөл',
+  title: 'ATM програм хийцгээе',
   image: "OIP.jpg",
   number: '105'
 }];
