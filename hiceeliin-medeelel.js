5311ebdfa554a2fecf676bf54a8149d6faac83e9
UrThings <160115501+UrThings@users.noreply.github.com>
2024-02-26 14:43:20 +0800

    Update hiceeliin-medeelel.js

--- hiceeliin-medeelel.js
+++ hiceeliin-medeelel.js
@@ -2,27 +2,27 @@ export const hiceeluud=[
   {
     name: '1р хичээл',
     title: 'C++ Гэж юу вэ? (Эхний хичээл)',
-    image: "dog-fire-house.png",
+    image: "What-is-c++.jpg",
     number: '1'
   },{
     name: '2р хичээл',
     title: 'Анхны программ',
-    image: "Quiz-Kid.jpg",
+    image: "What-is-c++.jpg",
     number: '2'
   },{
     name: '2р хичээл',
     title: 'Анхны программ',
-    image: "Quiz-Kid.jpg",
+    image: "What-is-c++.jpg",
     number: '3'
   },{
     name: '2р хичээл',
     title: 'Анхны программ',
-    image: "Quiz-Kid.jpg",
+    image: "What-is-c++.jpg",
     number: '4'
   },{
     name: '2р хичээл',
     title: 'Анхны программ',
-    image: "Quiz-Kid.jpg",
+    image: "What-is-c++.jpg",
     number: '5'
   }
 ];
