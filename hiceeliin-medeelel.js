a7f1f5152579306eaa5b54496a57ab1f86c1de95
UrThings <160115501+UrThings@users.noreply.github.com>
2024-02-26 15:03:17 +0800

    Update hiceeliin-medeelel.js

--- hiceeliin-medeelel.js
+++ hiceeliin-medeelel.js
@@ -6,7 +6,7 @@ export const hiceeluud=[
     number: '1'
   },{
     name: '2р хичээл',
-    title: 'Анхны программ',
+    title: 'Хөгжүүлэх орчин',
     image: "What-is-c++.jpg",
     number: '2'
   },{
@@ -16,13 +16,38 @@ export const hiceeluud=[
     number: '3'
   },{
     name: '2р хичээл',
-    title: 'Анхны программ',
+    title: '2 Тооны нийлбэр олох,
     image: "What-is-c++.jpg",
     number: '4'
   },{
     name: '2р хичээл',
-    title: 'Анхны программ',
+    title: 'Толгой файл',
     image: "What-is-c++.jpg",
     number: '5'
+  },{
+    name: '2р хичээл',
+    title: 'If нөхцөл шалгах үйлдэл',
+    image: "What-is-c++.jpg",
+    number: '6'
+  },{
+    name: '2р хичээл',
+    title: 'While нөхцөлт давталт гэж юу вэ?',
+    image: "What-is-c++.jpg",
+    number: '7'
+  },{
+    name: '2р хичээл',
+    title: 'For давталт',
+    image: "What-is-c++.jpg",
+    number: '8'
+  },{
+    name: '2р хичээл',
+    title: 'Хүндэвтэр бодлого бодож өөрийгөө сорих',
+    image: "What-is-c++.jpg",
+    number: '9'
+  },{
+    name: '2р хичээл',
+    title: 'Array массивын тухай',
+    image: "What-is-c++.jpg",
+    number: '10'
   }
 ];
