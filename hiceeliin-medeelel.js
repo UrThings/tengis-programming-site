480a18274fb5ed169c08251c501fdec679eaab80
UrThings <160115501+UrThings@users.noreply.github.com>
2024-05-05 22:18:52 +0800

    Update hiceeliin-medeelel.js

--- hiceeliin-medeelel.js
+++ hiceeliin-medeelel.js
@@ -3,53 +3,72 @@ export const hiceeluud=[
     name: '1р хичээл',
     title: 'C++ Гэж юу вэ? (Эхний хичээл)',
     image: "What-is-c++.jpg",
-    number: '1'
+    number: '1',
+    link: 'hicheel1.html'
   },{
     name: '2р хичээл',
     title: 'Хөгжүүлэх орчин',
     image: "What-is-c++.jpg",
-    number: '2'
+    number: '2',
+        link: 'hicheel2.html'
+
   },{
     name: '3р хичээл',
     title: 'Анхны программ',
     image: "What-is-c++.jpg",
-    number: '3'
+    number: '3',
+        link: 'hicheel3.html'
+
   },{
     name: '4р хичээл',
     title: '2 Тооны нийлбэр олох',
     image: "What-is-c++.jpg",
-    number: '4'
+    number: '4',
+        link: 'hicheel4.html'
+
   },{
     name: '5р хичээл',
     title: 'Толгой файл',
     image: "What-is-c++.jpg",
-    number: '5'
+    number: '5',
+        link: 'hicheel5.html'
+
   },{
     name: '6р хичээл',
     title: 'If нөхцөл шалгах үйлдэл',
     image: "What-is-c++.jpg",
-    number: '6'
+    number: '6',
+        link: 'hicheel6.html'
+
   },{
     name: '7р хичээл',
     title: 'For давталт',
     image: "What-is-c++.jpg",
-    number: '8'
+    number: '7',
+        link: 'hicheel7.html'
+
   },{
     name: '8р хичээл',
     title: 'Хүндэвтэр бодлого бодож өөрийгөө сорих',
     image: "What-is-c++.jpg",
-    number: '9'
+    number: '8',
+        link: 'hicheel8.html'
+
   }
   ,{
     name: '9р хичээл',
     title: 'Хүндэвтэр бодлого бодож өөрийгөө сорих 2',
     image: "What-is-c++.jpg",
-    number: '9'
+    number: '9',
+        link: 'hicheel9.html'
+
   }
   ,{
     name: '10р хичээл',
     title: 'Хүндэвтэр бодлого бодож өөрийгөө сорих 3',
     image: "What-is-c++.jpg",
-    number: '9'
+    number: '10',
+        link: 'hicheel10.html'
+
   }
 ];
